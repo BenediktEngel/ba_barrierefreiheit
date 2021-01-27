b7709e549b8af6368c8d7d6d7547e371df61aa91
benediktengel <ben.gel@web.de>
2021-01-27 21:48:38 +0100

    XooBlog - Fehler aus Prüfschritt 2.4.1a behoben.

--- Themes/xooblog/sidebar.php
+++ Themes/xooblog/sidebar.php
@@ -2,7 +2,8 @@
     <!-- Fehler in Prüfschritt 1.3.1a durch hinzufügen einer Überschrift für Screen-Reader behoben. -->
     <h2 class="screen-reader-text">Sidebar</h2>
     <!-- sidebar Here -->
-    <div class="tecxoo-sidebar sidebar ">
+    <!-- Fehler aus Prüfschritt 2.4.1a durch hinzufügen der passenden Landmark behoben -->
+    <div class="tecxoo-sidebar sidebar " role="complementary">
         <?php
 
         if (is_active_sidebar('sidebar-1')) :
